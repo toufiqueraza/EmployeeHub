b9b83cd90c0d2e5d3d1ce282b28824f9435dce70
Toufique Raza <info.toufiqueraza@gmail.com>
2026-09-07 21:49:06 +0530

    Configure media uploads and clean tracked cache files

--- config/settings.py
+++ config/settings.py
@@ -128,3 +128,6 @@ MAILERS = {
 }
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
--- config/urls.py
+++ config/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -19,3 +21,9 @@ urlpatterns = [
         include("employees.urls"),
     ),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT,
+    )
